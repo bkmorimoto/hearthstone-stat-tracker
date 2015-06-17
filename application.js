ba10b24210730cba3415be7fc166f389eebc9a3b
Ben Morimoto <bkmorimoto@gmail.com>
2015-06-17 15:24:52 -0400

    stub a pie chart to be displayed in modal

--- application.js
+++ application.js
@@ -16,6 +16,49 @@ if (Meteor.isClient) {
     }
   })
 
+  Template.navbar.helpers({
+    topGenresChart: function() {
+      return {
+        chart: {
+          plotBackgroundColor: null,
+          plotBorderWidth: null,
+          plotShadow: false
+        },
+        title: {
+          text: Meteor.user().profile.name + "'s top genres"
+        },
+        tooltip: {
+          pointFormat: '<b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+          pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+              enabled: true,
+              format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+              style: {
+                color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+              },
+              connectorColor: 'silver'
+            }
+          }
+        },
+        series: [{
+          type: 'pie',
+          name: 'genre',
+          data: [
+            ['Adventure',   45.0],
+            ['Action',       26.8],
+            ['Ecchi',   12.8],
+            ['Comedy',    8.5],
+            ['Yuri',     6.2]
+          ]
+        }]
+      };
+    }
+  });
+
   Template.navbar.events({
     'click .overall-stats': function() {
       $('.ui.modal').modal('show')
